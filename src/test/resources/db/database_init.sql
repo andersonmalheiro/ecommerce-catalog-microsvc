c5fb112f26739bd042c4c7afa3ffafb1aa54964d
Anderson Malheiro de Carvalho <anderson.malheiro@aluno.fapce.edu.br>
2021-12-29 13:49:22 -0300

    adding database init script for tests

---
+++ src/test/resources/db/database_init.sql
@@ -0,0 +1,10 @@
+create table product_vendors
+(
+    id serial
+        constraint product_vendors_pk
+            primary key,
+    name varchar not null,
+    description varchar,
+    created_at timestamp with time zone default CURRENT_TIMESTAMP,
+    updated_at timestamp with time zone default CURRENT_TIMESTAMP
+);
